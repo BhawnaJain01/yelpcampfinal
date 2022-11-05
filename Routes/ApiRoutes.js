dbe8d2b9f88adcd4b81694c27fff9d6dc1551b64
Sahil <sahilrohera10@gmail.com>
2022-11-05 17:28:24 +0530

    backend done

--- Routes/ApiRoutes.js
+++ Routes/ApiRoutes.js
@@ -3,8 +3,19 @@ const { Router } = require("express");
 const campgrounds = require("../controllers/campgrounds");
 const user = require("../controllers/user");
 
+//Campgrounds
 router.post("/AddCampgrounds", campgrounds.AddCampgrounds);
 router.get("/getCampGrounds", campgrounds.getCampGrounds);
+router.put("/editCampGrounds", campgrounds.editCampGrounds);
+router.delete("/deleteCampGrounds/:id", campgrounds.deleteCampGrounds);
+router.get("/getMyCampGrounds/:id", campgrounds.getMyCampGrounds);
+
+//Wishlist
+router.post("/AddToWishlist", campgrounds.AddToWishlist);
+router.get("/getWishlist/:userId", campgrounds.getWishlist);
+router.delete("/deleteFromWishlist/:id", campgrounds.deleteFromWishlist);
+
+//Users
 router.post("/register", user.register);
 router.post("/login", user.login);
 router.post("/contact", user.sendmailforcontact);
--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -4,6 +4,7 @@ const ObjectId = mongoose.Types.ObjectId;
 const path = require("path");
 
 const campgrounds = require("../models/campgrounds");
+const wishlist = require("../models/Wishlist");
 // const { User } = require("../models");
 const express = require("express");
 // const nodemailer = require("nodemailer");
@@ -12,9 +13,19 @@ const bcrypt = require("bcrypt");
 const { ObjectID } = require("bson");
 const multer = require("multer");
 
+const { baseAggregate } = require("./Repositories/baseRepository");
+
 module.exports = {
   AddCampgrounds,
   getCampGrounds,
+  editCampGrounds,
+  deleteCampGrounds,
+  getMyCampGrounds,
+
+  //wishlist
+  AddToWishlist,
+  getWishlist,
+  deleteFromWishlist,
 };
 
 async function AddCampgrounds(req, res, next) {
@@ -35,7 +46,6 @@ async function AddCampgrounds(req, res, next) {
       storage: storage,
       limits: { fileSize: maxSize },
       fileFilter: function (req, file, cb) {
-        // Set the filetypes, it is optional
         var filetypes = /jpeg|jpg|png/;
         var mimetype = filetypes.test(file.mimetype);
 
@@ -53,18 +63,12 @@ async function AddCampgrounds(req, res, next) {
             filetypes
         );
       },
-
-      // mypic is the name of file attribute
     }).single("image");
 
     upload(req, res, async function (err) {
       if (err) {
-        // ERROR occurred (here it can be occurred due
-        // to uploading image of size greater than
-        // 1MB or uploading different file type)
         return res.send(err);
       } else {
-        // productsData.update({ imageId: req.file.filename }, { where: { id: 2 } });
         const data = await campgrounds.create({
           imageId: req.file.filename,
           userId: req.body.userId,
@@ -76,8 +80,6 @@ async function AddCampgrounds(req, res, next) {
 
         console.log("data uploaded :", data);
         return res.status(200).json("camp ground Added successfully");
-        // SUCCESS, image successfully uploaded
-        // res.send("Success, Image uploaded!");
       }
     });
   } catch (error) {
@@ -93,5 +95,125 @@ async function getCampGrounds(req, res, next) {
     return res.status(200).json({ data });
   } catch (error) {
     return res.status(400).json(error);
+  }
+}
+
+async function editCampGrounds(req, res) {
+  try {
+    console.log("body=>", req.body);
+    const name = req.body.name;
+    const location = req.body.location;
+    const price = req.body.price;
+    const id = req.body.id;
+
+    const data = await campgrounds.updateOne(
+      { _id: id },
+      { name: name, location: location, price: price }
+    );
+
+    return res.status(200).json({ messgae: "success" });
+  } catch (error) {
+    console.log("error=>", error);
+    return res.status(400).json({ messgae: "failed" });
+  }
+}
+
+async function deleteCampGrounds(req, res) {
+  try {
+    const data = await campgrounds.deleteOne({ _id: req.params.id });
+
+    return res.status(200).json({ messgae: "success" });
+  } catch (error) {
+    console.log("error=>", error);
+    return res.status(400).json({ messgae: "failed" });
+  }
+}
+
+async function AddToWishlist(req, res) {
+  try {
+    const camp = await wishlist.findOne({
+      userId: req.body.userId,
+      campId: req.body.campId,
+    });
+
+    if (camp) {
+      return res.status(300).json({ message: "already in wishlist" });
+    } else {
+      const data = await wishlist.create(req.body);
+
+      return res.status(200).json({ message: "success" });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "failed" });
+  }
+}
+
+async function getWishlist(req, res) {
+  const params = new mongoose.Types.ObjectId(req.params.userId);
+  console.log("hi");
+
+  if (!params)
+    return res.status(400).json({ message: "userId is required in params" });
+
+  console.log("world");
+  console.log(params);
+
+  try {
+    let query = [
+      {
+        $match: {
+          userId: params,
+        },
+      },
+
+      {
+        $lookup: {
+          from: "campgrounds",
+          localField: "campId",
+          foreignField: "_id",
+          as: "camp",
+        },
+      },
+
+      {
+        $unwind: { path: "$camp", preserveNullAndEmptyArrays: false },
+      },
+
+      {
+        $project: {
+          _id: 1,
+          campId: "$camp._id",
+          imgId: "$camp.imageId",
+          name: "$camp.name",
+          price: "$camp.price",
+          location: "$camp.location",
+        },
+      },
+    ];
+
+    let data = await baseAggregate(wishlist, query);
+    console.log("data=>", data);
+    return res.status(200).json({ data });
+  } catch (error) {
+    return res.status(400).json({ mesage: "no data found" });
+  }
+}
+
+async function deleteFromWishlist(req, res) {
+  try {
+    const data = await wishlist.deleteOne({ _id: req.params.id });
+    return res.status(200).json({ message: "deleted" });
+  } catch (error) {
+    return res.status(400).json({ message: "failed" });
+  }
+}
+
+async function getMyCampGrounds(req, res) {
+  try {
+    const data = await campgrounds.find({ userId: req.params.id });
+
+    return res.status(200).json({ data: data });
+  } catch (error) {
+    return res.status(400).json({ data: "not found" });
   }
 }
